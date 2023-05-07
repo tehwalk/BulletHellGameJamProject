abe27ce2d7deb95915409b8a250c2a00a3583d00
Eirini_Al <eiraloupi2@gmail.com>
2023-05-07 20:48:46 +0300

    3rd commit

--- Assets/Scripts/Misc/MainMenuManager.cs
+++ Assets/Scripts/Misc/MainMenuManager.cs
@@ -5,9 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+    [SerializeField] GameObject creditsPanel;
+    private void Start()
+    {
+        HideCredits();
+    }
     public void PlayGame()
     {
         SceneManager.LoadScene((int)SceneIndex.Level1);
+    }
+
+    public void ShowCredits()
+    {
+        creditsPanel.SetActive(true);
+    }
+
+    public void HideCredits()
+    {
+        creditsPanel.SetActive(false);
     }
 
     public void Quit()
